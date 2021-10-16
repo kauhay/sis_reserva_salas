2ad4f46643f62cc00c538db5e7d35911aee4a7c9
Selton Silva <seltonkauhay@gamil.com>
2021-10-16 16:45:55 -0300

    Upload do repositorio

---
+++ login.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="pt-br" class="fullscreen-bg">
+
+<head>
+	<title>Login | Agenda</title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	<!-- VENDOR CSS -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/vendor/font-awesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/vendor/linearicons/style.css">
+
+	<link rel="stylesheet" href="assets/css/main.css">
+
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+	<!-- ICONS -->
+	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
+</head>
+
+<body>
+	<!-- WRAPPER -->
+	<div id="wrapper">
+		<div class="vertical-align-wrap">
+			<div class="vertical-align-middle">
+				<div class="auth-box ">
+					<div class="left">
+						<div class="content">
+							<div class="header">
+								<div class="logo text-center"><img src="assets/img/logo3.png" alt="Aniger"></div>
+								<p class="lead">Sistema de Reservas de Salas</p>
+							</div>
+							<form class="form-auth-small"action="autenticar.php">
+								<div class="form-group">
+	            <label for="signin-email" class="control-label sr-only">Login</label>
+	            <input class="form-control" name="login" id="exampleInputEmail1" type="text" placeholder="Login">
+	          </div>
+						<div class="form-group">
+							<label for="signin-password" class="control-label sr-only">Senha</label>
+							<input class="form-control" name="senha" id="exampleInputPassword1" type="password" placeholder="Senha">
+							</div>
+								<div class="form-group clearfix">
+									<label class="fancy-checkbox element-left">
+
+								</div>
+								<button type="submit" class="btn btn-primary btn-lg btn-block">LOGIN</button>
+								<!--<div class="bottom">
+									<span class="helper-text"><i class="fa fa-lock"></i> <a href="#">Forgot password?</a></span>
+								</div>-->
+							</form>
+						</div>
+					</div>
+					<div class="right">
+						<div class="overlay"></div>
+						<div class="content text">
+							<h1 class="">Sistema de Reservas de Salas</h1>
+							<p>Minha Empresa </p>
+						</div>
+					</div>
+					<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- END WRAPPER -->
+</body>
+
+</html>
